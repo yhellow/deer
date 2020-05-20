ef83ffdb96ca06ee674f95070a0abad1fc3d23d5
yhellow <61879663+yhellow@users.noreply.github.com>
2020-05-20 15:02:05 +0900

    grammar0520

--- oreilly/grammar/grammar2.py
+++ oreilly/grammar/grammar2.py
@@ -6,9 +6,25 @@
 # lst = list(map(func,nums))
 # print(lst)		# [2,3,4,5,6]
 
-def mapper(x):
-    return x*x
+# def mapper(x):
+#     return x*x
 
-xs = [1,2,3,4,5,6]
-mapped = map(mapper, xs)
-print(set(mapped))
+# xs = [1,2,3,4,5,6]
+# mapped = map(mapper, xs)
+# print(set(mapped))
+
+# class Puppy :
+#   def setName(self,_name) :
+#     self.name = _name
+  
+# myPet = Puppy()
+# myPet.setName("꽁별이")
+# print(myPet.name)	
+
+class phones:
+  def os(self, system):
+    self.whatos = system
+
+iphone = phones()
+iphone.os("ios")
+print(iphone.whatos)
--- oreilly/practice.py
+++ oreilly/practice.py
@@ -1,4 +1,4 @@
 #!/usr/local/bin/python3
 
-# l = [1,2,3,4]
-# print(l)
+l = [1,2,3,4]
+print(l)
