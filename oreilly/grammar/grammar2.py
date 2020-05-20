f9ddd99b025cba85018abce5fdeedafa4c869e85
yhellow <61879663+yhellow@users.noreply.github.com>
2020-05-20 22:03:40 +0900

    grammar0520

--- oreilly/grammar/grammar2.py
+++ oreilly/grammar/grammar2.py
@@ -21,10 +21,62 @@
 # myPet.setName("꽁별이")
 # print(myPet.name)	
 
-class phones:
-  def os(self, system):
-    self.whatos = system
+# class phones:
+#   def os(self, system):
+#     self.whatos = system
 
-iphone = phones()
-iphone.os("ios")
-print(iphone.whatos)
+# iphone = phones()
+# iphone.os("ios")
+# print(iphone.whatos)
+
+# class Puppy :
+#   def __init__(self,_age,name='댕댕이') :
+#     self.age = _age
+    
+# myPet = Puppy(5)
+# print("%s, %d세" %(myPet.name,myPet.age))
+
+# class iphone: 
+#   def atti(self, gb, os='ios'):
+#     self.gbis = gb
+#     self.osis = os
+
+# myphone = iphone()
+# myphone.atti(128)
+# print("my phone runs on %s and is %d gb" %(myphone.osis, myphone.gbis))
+
+
+# class Chicken :
+#   count = 0
+#   def __init__(self,_brand='No brand') :
+#     self.brand = _brand
+#     Chicken.count += 1
+
+# order1 = Chicken('굽네')
+# order2 = Chicken('교촌')
+
+# # print(Chicken.count)		# 2
+# print(order2.count)		# 2
+
+# class Phones:
+#   def whatphone(self,os,gb):
+#     self.whatos = os
+#     self.whatgb = gb
+    
+# phone1 = Phones('apple')
+# whatphone.os = ios
+# whatphone.gb = 128
+# phone1 = Phones('samsung')
+# whatphone.os = android
+# whatphone.gb = 256
+
+# print(phone1.os)
+
+
+# try : 
+#   1/0
+#   # raise ZeroDivisionError	# 오류 발생
+# except ZeroDivisionError as e :
+#   print(e)
+# else :
+#   print("Success!")
---
+++ oreilly/grammar/testmodule.py
@@ -0,0 +1,7 @@
+def multiplyer(x):
+    return x *10
+
+def adder(x):
+    return x +10
+
+print(adder(2))
