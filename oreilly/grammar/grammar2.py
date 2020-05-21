c340312909220409bb97f08e2174f982449d8f43
yhellow <61879663+yhellow@users.noreply.github.com>
2020-05-21 23:32:26 +0900

    pyenv0521

--- oreilly/grammar/grammar2.py
+++ oreilly/grammar/grammar2.py
@@ -1,82 +1,87 @@
-# def func(num) :
-#   return num+1
+def func(num) :
+  return num+1
 
-# nums = [1,2,3,4,5]
+nums = [1,2,3,4,5]
 
-# lst = list(map(func,nums))
-# print(lst)		# [2,3,4,5,6]
+lst = list(map(func,nums))
+print(lst)		# [2,3,4,5,6]
 
-# def mapper(x):
-#     return x*x
+def mapper(x):
+    return x*x
 
-# xs = [1,2,3,4,5,6]
-# mapped = map(mapper, xs)
-# print(set(mapped))
+xs = [1,2,3,4,5,6]
+mapped = map(mapper, xs)
+print(set(mapped))
 
-# class Puppy :
-#   def setName(self,_name) :
-#     self.name = _name
+class Puppy :
+  def setName(self,_name) :
+    self.name = _name
   
-# myPet = Puppy()
-# myPet.setName("꽁별이")
-# print(myPet.name)	
+myPet = Puppy()
+myPet.setName("꽁별이")
+print(myPet.name)	
 
-# class phones:
-#   def os(self, system):
-#     self.whatos = system
+class phones:
+  def os(self, system):
+    self.whatos = system
 
-# iphone = phones()
-# iphone.os("ios")
-# print(iphone.whatos)
+iphone = phones()
+iphone.os("ios")
+print(iphone.whatos)
 
-# class Puppy :
-#   def __init__(self,_age,name='댕댕이') :
-#     self.age = _age
+class Puppy :
+  def __init__(self,_age,name='댕댕이') :
+    self.age = _age
     
-# myPet = Puppy(5)
-# print("%s, %d세" %(myPet.name,myPet.age))
+myPet = Puppy(5)
+print("%s, %d세" %(myPet.name,myPet.age))
 
-# class iphone: 
-#   def atti(self, gb, os='ios'):
-#     self.gbis = gb
-#     self.osis = os
+class iphone: 
+  def atti(self, gb, os='ios'):
+    self.gbis = gb
+    self.osis = os
 
-# myphone = iphone()
-# myphone.atti(128)
-# print("my phone runs on %s and is %d gb" %(myphone.osis, myphone.gbis))
+myphone = iphone()
+myphone.atti(128)
+print("my phone runs on %s and is %d gb" %(myphone.osis, myphone.gbis))
 
 
-# class Chicken :
-#   count = 0
-#   def __init__(self,_brand='No brand') :
-#     self.brand = _brand
-#     Chicken.count += 1
+class Chicken :
+  count = 0
+  def __init__(self,_brand='No brand') :
+    self.brand = _brand
+    Chicken.count += 1
 
-# order1 = Chicken('굽네')
-# order2 = Chicken('교촌')
+order1 = Chicken('굽네')
+order2 = Chicken('교촌')
 
-# # print(Chicken.count)		# 2
-# print(order2.count)		# 2
+# print(Chicken.count)		# 2
+print(order2.count)		# 2
 
-# class Phones:
-#   def whatphone(self,os,gb):
-#     self.whatos = os
-#     self.whatgb = gb
+class Phones:
+  def whatphone(self,os,gb):
+    self.whatos = os
+    self.whatgb = gb
     
-# phone1 = Phones('apple')
-# whatphone.os = ios
-# whatphone.gb = 128
-# phone1 = Phones('samsung')
-# whatphone.os = android
-# whatphone.gb = 256
+phone1 = Phones('apple')
+whatphone.os = ios
+whatphone.gb = 128
+phone1 = Phones('samsung')
+whatphone.os = android
+whatphone.gb = 256
 
-# print(phone1.os)
+print(phone1.os)
+
+try :
+  1/0
+  # raise ZeroDivisionError	# 오류 발생
+except ZeroDivisionError as e :
+  print(e)
+else :
+  print("Success!")
 
 
-# try : 
-#   1/0
-#   # raise ZeroDivisionError	# 오류 발생
-# except ZeroDivisionError as e :
-#   print(e)
-# else :
-#   print("Success!")
+try:
+except:
+else:
+finally;
