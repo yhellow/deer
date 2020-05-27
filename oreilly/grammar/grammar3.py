af0b625b15383c2950bf1e151c06340274c1b28e
yhellow <61879663+yhellow@users.noreply.github.com>
2020-05-28 01:46:53 +0900

    pysetup guidenotes

---
+++ oreilly/grammar/grammar3.py
@@ -0,0 +1,17 @@
+"editor.tokenColorCustomizations": {
+    "comments": "#12CBC4",
+    "textMateRules": [
+      {
+        "scope": "string.quoted.docstring.multi.python",
+        "settings": {
+          "foreground": "#f1dd83"
+        }
+      }
+    ]
+  },
+
+"""
+write a meaningful docstring
+"""
+    """autoDocstring
+    """
