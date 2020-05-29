2bcf12221512e482c0eb5b70fc96396f08f6828c
yhellow <61879663+yhellow@users.noreply.github.com>
2020-05-29 20:19:51 +0900

    pyfreecodecamp2

--- oreilly/grammar/grammar3.py
+++ oreilly/grammar/grammar3.py
@@ -17,45 +17,84 @@
 # autoDocstring
 # """
 
-# guessing game1
-# guess = ""
-# answer = "cheater"
-# while guess != answer:
-#   guess = input('guess the answer! : ') 
+# guessing game 1
+guess = ""
+answer = "cheater"
+while guess != answer:
+  guess = input('guess the answer! : ') 
 
-# print('guessed correctly')
+print('guessed correctly')
 
-# # guessing game2
-# answer = "cheater"
-# guess = input("guess the answer: ")
-# guessCount = 0
+# # guessing game 2
+answer = "cheater"
+guess = input("guess the answer: ")
+guessCount = 0
 
-# while guess != answer and guessCount < 3:
-#   guess = input("keep guessing: ")
-#   guessCount += 1
+while guess != answer and guessCount < 3:
+  guess = input("keep guessing: ")
+  guessCount += 1
 
-# if guessCount == 3:
-#   print("you're out of guesses!")
-# else: 
-#   print("you guessed correctly!")
+if guessCount == 3:
+  print("you're out of guesses!")
+else: 
+  print("you guessed correctly!")
 
-# rdm = ['df', 'dfs', 'sdfsdf', 'asf']
-# rdmlength = len(rdm)
+rdm = ['df', 'dfs', 'sdfsdf', 'asf']
+rdmlength = len(rdm)
 
-# for r in range(rdmlength):
-#   print (rdm[r])
+for r in range(rdmlength):
+  print (rdm[r])
 
-# power function1
-# def powerfunc(a,b):
-#   return a**b
-# print(powerfunc(2,2))
+# power function 1
+def powerfunc(a,b):
+  return a**b
+print(powerfunc(2,2))
 
-# power function2
-
+# power function 2
 def powerfunc(a, b):
   anum = 1
   for bnum in range(b):
     anum = anum * a
   return anum
-
 print(powerfunc(7,3))
+
+# grid list
+gridlike = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+for row in gridlike:
+  for col in row:
+    print(col)
+
+for row in gridlike:
+  print(row[1]
+
+# translator for vowels are v 1
+def translator(phrase):
+  translation = ''
+  for letter in phrase:
+    if letter in "AEIOU":
+      translation = translation + "V"
+    elif letter in "aeiou":
+      translation  = translation + "v"
+    else:
+      translation = translation + letter
+  return translation
+print(translator(input('insert what you want to translate: ')))
+
+# translator for vowels are v 2
+def translator(phrase):
+  phrasel = list(phrase)
+  for index in range(len(phrase)):
+    if phrasel[index] in "AEIOU":
+      phrasel[index] = 'V'
+    elif phrasel[index] in "aeiou":
+      phrasel[index] = 'v'
+    else:
+      continue
+  return phrasel
+
+print(('').join(translator(input("insert phrase to translate: "))))
